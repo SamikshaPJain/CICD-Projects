417b9a55f688101fa780dafcb7420827f0030e3c
SamikshaPJain <140726807+SamikshaPJain@users.noreply.github.com>
2024-06-26 21:10:19 +0530

    Update createSafeContext.ts

--- Netflix_Clone/src/lib/createSafeContext.ts
+++ Netflix_Clone/src/lib/createSafeContext.ts
@@ -1,37 +1,15 @@
-import { Outlet, useLocation, useNavigation } from "react-router-dom";
-import Box from "@mui/material/Box";
+import React from "react";
 
-import DetailModal from "src/components/DetailModal";
-import VideoPortalContainer from "src/components/VideoPortalContainer";
-import DetailModalProvider from "src/providers/DetailModalProvider";
-import PortalProvider from "src/providers/PortalProvider";
-import { MAIN_PATH } from "src/constant";
-import { Footer, MainHeader } from "src/components/layouts";
-import MainLoadingScreen from "src/components/MainLoadingScreen";
+export default function createSafeContext<TValue extends {} | null>() {
+  const context = React.createContext<TValue | undefined>(undefined);
 
-export default function MainLayout() {
-  const location = useLocation();
-  const navigation = useNavigation();
-  // console.log("Nav Stat: ", navigation.state);
-  return (
-    <Box
-      sx={{
-        width: "100%",
-        minHeight: "100vh",
-        bgcolor: "background.default",
-      }}
-    >
-      <MainHeader />
-      {navigation.state !== "idle" && <MainLoadingScreen />}
-      <DetailModalProvider>
-        <DetailModal />
-        <PortalProvider>
-          {/* <MainLoadingScreen /> */}
-          <Outlet />
-          <VideoPortalContainer />
-        </PortalProvider>
-      </DetailModalProvider>
-      {location.pathname !== `/${MAIN_PATH.watch}` && <Footer />}
-    </Box>
-  );
+  function useContext() {
+    const value = React.useContext(context);
+    if (value === undefined) {
+      throw new Error("useContext must be inside a Provider with a value");
+    }
+    return value;
+  }
+
+  return [useContext, context.Provider] as const;
 }
